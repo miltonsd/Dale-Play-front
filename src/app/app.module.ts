0b78b7002a6a74422b0a167e5cf0c2a569a8d286
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2022-08-28 03:04:43 -0300

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ComponentsModule } from './shared/modules';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ComponentsModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/shared/modules/material/material.module.ts
+++ src/app/shared/modules/material/material.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatButtonModule } from '@angular/material/button';
+
+const modules = [MatButtonModule];
 @NgModule({
   declarations: [],
   imports: [CommonModule],
+  exports: [...modules],
 })
 export class MaterialModule {}
