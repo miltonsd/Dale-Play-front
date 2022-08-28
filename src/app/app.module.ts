667c7d8d6de2169eacc262b8b10990913f99ce2d
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2022-08-28 20:46:45 -0300

    add: header and footer components; update: material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Importación del Header y el Footer
+import { HeaderComponent, FooterComponent } from '@dlp/core/components';
+// Módulos compartidos
 import { ComponentsModule } from './shared/modules';
 
+const components = [HeaderComponent, FooterComponent];
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ...components],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/shared/modules/material/material.module.ts
+++ src/app/shared/modules/material/material.module.ts
@@ -1,9 +1,42 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Módulos de Angular Material
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatMenuModule } from '@angular/material/menu';
 
-const modules = [MatButtonModule];
+const modules = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatSlideToggleModule,
+  MatIconModule,
+  MatTooltipModule,
+  MatSidenavModule,
+  MatListModule,
+  MatDividerModule,
+  MatTabsModule,
+  MatCardModule,
+  MatChipsModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSnackBarModule,
+  MatProgressSpinnerModule,
+  MatMenuModule,
+];
 @NgModule({
   declarations: [],
   imports: [CommonModule],
