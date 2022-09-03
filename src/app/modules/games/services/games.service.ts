bd304f9e17c3d590a95acaa527e528c030962270
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2022-09-03 18:39:21 -0300

    game-details show info

--- src/app/modules/games/services/games.service.ts
+++ src/app/modules/games/services/games.service.ts
@@ -22,29 +22,8 @@ export class GamesService {
   }
 
   getGames() {
-    return this._http
-      .get(`${environment.apiUrl}/game/`, { headers: this.headers })
-      .pipe(
-        map((response: any) => {
-          const games: Game[] = [];
-          if (response.elemts) {
-            response.elemts.map((gameItem: any) => {
-              const game: Game = {
-                id: gameItem.id || 0,
-                name: gameItem.name || '',
-                image: gameItem.image || '',
-                valoration: gameItem.valoration || 0,
-                idCategory: gameItem.idCategory || 0,
-                idDeveloper: gameItem.idDeveloper || 0,
-                description: gameItem.description || '',
-                createdAt: gameItem.createdAt || '',
-                updatedAt: gameItem.updatedAt || '',
-              };
-              games.push(game);
-            });
-          }
-          return games;
-        })
-      );
+    return this._http.get(`${environment.apiUrl}/game/`, {
+      headers: this.headers,
+    });
   }
 }
