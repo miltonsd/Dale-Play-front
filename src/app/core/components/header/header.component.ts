cb40f95cc51499376d9f0d87204a5e2f1fbf5dc2
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2022-09-03 02:44:38 -0300

    add logout button

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/modules/auth/services/auth.service';
 
 @Component({
   selector: 'dlp-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor(private _authService: AuthService, private _router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onClick(): void {
+    console.log('Click');
+    this._authService.logout();
+    this._router.navigate(['/login']);
   }
-
 }
