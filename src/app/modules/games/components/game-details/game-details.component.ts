52d0bd22bdf5e935bfcf2db1dfbd541943263aee
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2022-09-04 02:13:29 -0300

    update games details

--- src/app/modules/games/components/game-details/game-details.component.ts
+++ src/app/modules/games/components/game-details/game-details.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { GamesService } from '@dlp/games/services';
 import { DevelopersService } from '@dlp/devs/services';
+import { CategoriesService } from '@dlp/categories/services';
 
 @Component({
   selector: 'dlp-game-details',
@@ -15,6 +16,7 @@ export class GameDetailsComponent implements OnInit {
     private _activatedRoute: ActivatedRoute,
     private _gamesService: GamesService,
     private _developesrService: DevelopersService,
+    private _categoriesService: CategoriesService,
     private _router: Router
   ) {}
 
@@ -28,18 +30,31 @@ export class GameDetailsComponent implements OnInit {
               .getDeveloper(res.elemnt.idDeveloper)
               .subscribe({
                 next: (resDev: any) => {
-                  this.game = {
-                    id: res.elemnt.id,
-                    name: res.elemnt.name,
-                    description: res.elemnt.description,
-                    image: res.elemnt.image,
-                    developer: resDev.elemnt.name,
-                    idCategory: res.elemnt.idCategory,
-                    valoration: res.elemnt.valoration,
-                    createdAt: res.elemnt.createdAt,
-                    updateAt: res.elemnt.updatedAt,
-                  };
-                  console.log(this.game);
+                  this._categoriesService
+                    .getCategory(res.elemnt.idCategory)
+                    .subscribe({
+                      next: (resCat: any) => {
+                        this.game = {
+                          id: res.elemnt.id,
+                          name: res.elemnt.name,
+                          description: res.elemnt.description,
+                          image: res.elemnt.image,
+                          developer: resDev.elemnt.name,
+                          category: resCat.elemnt.name,
+                          valoration: res.elemnt.valoration,
+                          createdAt: res.elemnt.createdAt,
+                          updateAt: res.elemnt.updatedAt,
+                        };
+                        console.log(this.game);
+                      },
+                      error: (err) => {
+                        console.error(
+                          `Código de error ${err.status}: `,
+                          err.error.msg
+                        );
+                        this._router.navigate(['/store']);
+                      },
+                    });
                 },
                 error: (err) => {
                   console.error(
