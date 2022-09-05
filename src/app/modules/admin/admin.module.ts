2f640f3e83cde73737b0d2d6a9a202aa86cc5613
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2022-09-05 14:54:59 -0300

    add sidebar in admin view

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -4,14 +4,12 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 
+// Modulos
+import { ComponentsModule, MaterialModule } from '@dlp/shared/modules';
 
+const modules = [ComponentsModule, MaterialModule];
 @NgModule({
-  declarations: [
-    AdminComponent
-  ],
-  imports: [
-    CommonModule,
-    AdminRoutingModule
-  ]
+  declarations: [AdminComponent],
+  imports: [CommonModule, AdminRoutingModule, ...modules],
 })
-export class AdminModule { }
+export class AdminModule {}
