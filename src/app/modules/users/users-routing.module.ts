721e3ae38c53892e5aa9b6de444107e54ef5ea17
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2022-09-04 22:42:56 -0300

    add profile view

--- src/app/modules/users/users-routing.module.ts
+++ src/app/modules/users/users-routing.module.ts
@@ -2,7 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersComponent } from './users.component';
 
-const routes: Routes = [{ path: '', component: UsersComponent }];
+// Vistas
+import { ProfileComponent } from '@dlp/users/views';
+// Guards
+import { AuthGuard } from '@dlp/shared/guards';
+
+const routes: Routes = [
+  {
+    path: '',
+    canActivate: [AuthGuard],
+    component: ProfileComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -5,9 +5,10 @@ import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
 
 import { ComponentsModule, MaterialModule } from '@dlp/shared/modules';
+import { ProfileComponent } from './views/profile/profile.component';
 
 @NgModule({
-  declarations: [UsersComponent],
+  declarations: [UsersComponent, ProfileComponent],
   imports: [CommonModule, UsersRoutingModule, ComponentsModule, MaterialModule],
 })
 export class UsersModule {}
