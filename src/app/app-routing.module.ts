baad88a70e0820823225e76a490ec9b30ce797d6
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2022-09-08 18:16:45 -0300

    add home view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,11 @@ import { AdminGuard, AuthGuard } from '@dlp/shared/guards';
 
 const routes: Routes = [
   {
+    path: 'home',
+    loadChildren: () =>
+      import('./modules/home/home.module').then((m) => m.HomeModule),
+  },
+  {
     path: '',
     loadChildren: () =>
       import('./modules/auth/auth.module').then((m) => m.AuthModule),
