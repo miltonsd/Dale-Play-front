d70b7a687efc3f0a9ad5c904a7aff5d7235205ea
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2022-09-09 23:14:01 -0300

    add filter store by name

--- src/app/modules/games/views/store/store.component.ts
+++ src/app/modules/games/views/store/store.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { GamesService } from '@dlp/games/services';
 import { Game } from '@dlp/games/models';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'dlp-store',
@@ -10,6 +11,7 @@ import { Game } from '@dlp/games/models';
 })
 export class StoreComponent implements OnInit {
   games: Game[] = [];
+  dataSource!: MatTableDataSource<any>;
 
   constructor(private _gamesService: GamesService) {}
 
@@ -17,13 +19,22 @@ export class StoreComponent implements OnInit {
     // Busca los juegos para cargarlos en la tienda
     this._gamesService.getGames().subscribe({
       next: (response: any) => {
-        console.log(response);
-
         this.games = response;
       },
-      error: (err) => {
+      error: (err: any) => {
         console.error(`Código de error ${err.status}: `, err.error.msg);
       },
+      complete: () => {
+        this.dataSource = new MatTableDataSource(this.games);
+        this.dataSource.filterPredicate = (data, filter) => {
+          return data.name.toLowerCase().includes(filter);
+        };
+      },
     });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
