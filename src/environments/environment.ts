20504ad639d724ab87044e16ad558a808e878977
Luchoragusa <luchito.rc4@gmail.com>
2022-09-12 21:02:25 -0300

    apiUrl update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
-  // apiUrl: 'http://ljragusa.us:3000',
+  //apiUrl: 'http://localhost:3000',
+  apiUrl: 'http://ljragusa.us:3000',
 };
 
 /*
